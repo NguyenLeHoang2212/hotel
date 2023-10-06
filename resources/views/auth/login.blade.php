b197019500129f80225fc7236b1370d893e6315b
NguyenLeHoang2212 <hoang19992212@gmail.com>
2023-10-06 16:37:41 +0700

    update 6/10

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,233 +1,249 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-    <!-- Mobile Web-app fullscreen -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-
-    <!-- Meta tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!--Title-->
-    <title>Colina - Hotel, Resort</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
-    <!--CSS styles-->
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/bootstrap.css') }}" />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/animate.css') }}"  />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/awesome.css') }}"  />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/linear-icons.css') }}" />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/hotel-icons.css') }}"  />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/magnific-popup.css') }}" />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/owl.carousel.css') }}"  />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/datepicker.css') }}"  />
-    <link rel="stylesheet" media="all" href="{{ asset('assets/client/css/theme.css') }}"  />
-
-    <!--Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,500&amp;subset=latin-ext" rel="stylesheet">
-
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&amp;subset=latin-ext" rel="stylesheet">
-
-    <style>
-        .loginuser{
-            display: flex;
-
-        }
-        .left{
-            width: 50%;
-            margin-top: 100px;
-            text-align: center;
-        }
-        .left .formlogin{
-            width: 60%;
-            margin: 70px auto;
-            text-align: center;
-
-        }
-        .right{
-            width: 50%;
-        }
-        .right img{
-            height: 100vh;
-            object-fit: cover;
-            width: 100%;
-        }
-        .formlogin .first{
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-        }
-        .formlogin  .last{
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-
-        }
-        .formlogin .first input{
-            width: 60%;
-            margin-right: 50px;
-            padding: 20px;
-
-        }
-        .formlogin .first label{
-            width: 20%
-        }
-        .formlogin .last label{
-            width: 20%
-        }
-        .formlogin .last input{
-            width: 60%;
-            margin-right: 50px;
-            padding: 20px;
-        }
-        .controlform{
-            display: flex;
-            justify-content: space-around;
-        }
-        .buttonlogin{
-            display: flex;
-            justify-content: space-around;
-            margin: 50px auto;
-
-        }
-        .contaainer {
-  display: block;
-  position: relative;
-    margin-bottom: 12px;
-    margin-left: 100px;
-
-    padding-right: 100px;
-  cursor: pointer;
-  font-size: 15px;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<style>
+    body
+{
+    background: url('http://127.0.0.1:8000/assets/client/images/slide-3.jpg') fixed;
+    background-size: cover;
+    padding: 0;
+    margin: 0;
 }
 
-/* Hide the browser's default checkbox */
-.contaainer input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
+.wrap
+{
+    width: 100%;
+    height: 100%;
+    min-height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 99;
 }
 
-/* Create a custom checkbox */
-.checkmark {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 20px;
-  width: 20px;
-  background-color: #eee;
+p.form-title
+{
+    font-family: 'Open Sans' , sans-serif;
+    font-size: 20px;
+    font-weight: 600;
+    text-align: center;
+    color: #FFFFFF;
+    margin-top: 5%;
+    text-transform: uppercase;
+    letter-spacing: 4px;
 }
 
-/* On mouse-over, add a grey background color */
-.contaainer:hover input ~ .checkmark {
-  background-color: #ccc;
+form
+{
+    width: 400px;
+    margin: 50px auto;
 }
 
-/* When the checkbox is checked, add a blue background */
-.contaainer input:checked ~ .checkmark {
-  background-color: #2196F3;
+form.login input[type="text"], form.login input[type="password"]
+{
+    width: 100%;
+    margin: 0;
+    padding: 5px 10px;
+    background: 0;
+    border: 0;
+    border-bottom: 1px solid #FFFFFF;
+    outline: 0;
+    font-style: italic;
+    font-size: 12px;
+    font-weight: 400;
+    letter-spacing: 1px;
+    margin-bottom: 5px;
+    color: #FFFFFF;
+    outline: 0;
 }
 
-/* Create the checkmark/indicator (hidden when not checked) */
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
+form.login input[type="submit"]
+{
+    width: 100%;
+    font-size: 14px;
+    text-transform: uppercase;
+    font-weight: 500;
+    margin-top: 16px;
+    outline: 0;
+    cursor: pointer;
+    letter-spacing: 1px;
 }
 
-/* Show the checkmark when checked */
-.contaainer input:checked ~ .checkmark:after {
-  display: block;
+form.login input[type="submit"]:hover
+{
+    transition: background-color 0.5s ease;
 }
 
-/* Style the checkmark/indicator */
-.contaainer .checkmark:after {
-  left: 8px;
-  top: 5px;
-  width: 5px;
-  height: 9px;
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  -webkit-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  transform: rotate(45deg);
+form.login .remember-forgot
+{
+    float: left;
+    width: 100%;
+    margin: 10px 0 0 0;
 }
-    </style>
-</head>
-<body>
-    <div class="loginuser">
-        <div class="left">
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+form.login .forgot-pass-content
+{
+    min-height: 20px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+form.login label, form.login a
+{
+    font-size: 12px;
+    font-weight: 400;
+    color: #FFFFFF;
+}
 
-        <h1>LOGIN TO COLINA</h1>
+form.login a
+{
+    transition: color 0.5s ease;
+}
 
-        <form class="formlogin" method="POST" action="{{ route('login') }}">
-            @csrf
+form.login a:hover
+{
+    color: #2ecc71;
+}
 
-            <!-- Email Address -->
-            <div class="form-group first" >
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+.pr-wrap
+{
+    width: 100%;
+    height: 100%;
+    min-height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    display: none;
+}
+
+.show-pass-reset
+{
+    display: block !important;
+}
+
+.pass-reset
+{
+    margin: 0 auto;
+    width: 250px;
+    position: relative;
+    margin-top: 22%;
+    z-index: 999;
+    background: #FFFFFF;
+    padding: 20px 15px;
+}
+
+.pass-reset label
+{
+    font-size: 12px;
+    font-weight: 400;
+    margin-bottom: 15px;
+}
+
+.pass-reset input[type="email"]
+{
+    width: 100%;
+    margin: 5px 0 0 0;
+    padding: 5px 10px;
+    background: 0;
+    border: 0;
+    border-bottom: 1px solid #000000;
+    outline: 0;
+    font-style: italic;
+    font-size: 12px;
+    font-weight: 400;
+    letter-spacing: 1px;
+    margin-bottom: 5px;
+    color: #000000;
+    outline: 0;
+}
+
+.pass-reset input[type="submit"]
+{
+    width: 100%;
+    border: 0;
+    font-size: 14px;
+    text-transform: uppercase;
+    font-weight: 500;
+    margin-top: 10px;
+    outline: 0;
+    cursor: pointer;
+    letter-spacing: 1px;
+}
+
+.pass-reset input[type="submit"]:hover
+{
+    transition: background-color 0.5s ease;
+}
+.posted-by
+{
+    position: absolute;
+    bottom: 26px;
+    margin: 0 auto;
+    color: #FFF;
+    background-color: rgba(0, 0, 0, 0.66);
+    padding: 10px;
+    left: 45%;
+}
+
+
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="pr-wrap">
+                <div class="pass-reset">
+                    <label>
+                        Enter the email you signed up with</label>
+                    <input type="email" placeholder="Email" />
+                    <input type="submit" value="Submit" class="pass-reset-submit btn btn-success btn-sm" />
+                </div>
             </div>
-
-            <!-- Password -->
-            <div class="form-group last mb-3">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-
-
-                {{-- <label for="remember_me" class="inline-flex items-center">
-                    <input >
-                    <span class="ml-2 text-sm text-gray-600"></span>
-                </label> --}}
-                <label for="remember_me" class="contaainer">{{ __('Remember me') }}
-                    <input id="remember_me" type="checkbox"  name="remember" >
-                    <span class="checkmark"></span>
-                  </label>
-
-
-
-
-            <div class="buttonlogin">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-primary-button  class="ml-3 btn btn-primary">
+            <div class="wrap">
+                <p class="form-title">
+                    Sign In</p>
+                <form  method="POST" action="{{ route('login') }}" class="login">
+                    @csrf
+                <input name="email" :value="old('email')" required autofocus autocomplete="username"  type="text" placeholder="Username" />
+                <input type="password"
+                name="password"
+                required autocomplete="current-password"  placeholder="Password" />
+                <x-primary-button style=" width: 100%;
+                font-size: 14px;
+                text-transform: uppercase;
+                font-weight: 500;
+                margin-top: 16px;
+                outline: 0;
+                cursor: pointer;
+                letter-spacing: 1px;"
+                class="btn btn-success btn-sm">
                     {{ __('Log in') }}
                 </x-primary-button>
-            </div>
-        </form>
-        </div>
-        <div class="right">
-            <img  src="{{ asset('assets/client/images/slide-3.jpg') }}"/>
-        </div>
+                                <div class="remember-forgot">
+                    <div class="row">
 
+                        <div class="col-md-6">
+                            <div class="checkbox">
+                                <label>
+                                    <input  name="remember" type="checkbox" />
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-md-6 forgot-pass-content">
+                            @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif
+                        </div>
+                    </div>
+                    <div style="text-align:center;"><a  href="register">Register</a></div>
+
+                </div>
+                </form>
+            </div>
+        </div>
     </div>
-</body>
-</html>
+</div>
 
